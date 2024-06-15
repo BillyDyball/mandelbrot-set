5ab4dfb1b9e110528d9c37f2fcd31c35ec9e1b97
BillyDyball <47304846+BillyDyball@users.noreply.github.com>
2024-06-15 22:35:14 +0100

    Updated color gradient WIP

--- src/components/ColorGradient.tsx
+++ src/components/ColorGradient.tsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from "react";
 import { ColorStop } from "../types/common.types";
 
 interface ColorGradientProps {
@@ -5,29 +6,47 @@ interface ColorGradientProps {
 }
 
 function ColorGradient({ colors }: ColorGradientProps) {
+  const [selected, setSelected] = useState<number>(0);
+
+  const gradientRef = useRef<HTMLDivElement>(null);
+
+  const getTranslate = (stop: number): number => {
+    if (!gradientRef.current) return 0;
+    return gradientRef.current.clientWidth * stop - stop * 32;
+  };
+
+  const handleColorAdd = () => {};
+
   return (
-    <div
-      className="w-full h-8 rounded-xl relative"
-      style={{
-        background: `linear-gradient(to right, ${colors
-          .map(({ hex, stop }) => `${hex} ${stop * 100}%`)
-          .join(",")})`,
-      }}
-    >
-      {colors.map(({ hex, stop }) => (
-        <div
-          className={`h-8 w-2 absolute bg-gray-500 border border-gray-200 hover:animate-pulse cursor-pointer`}
-          style={{
-            left: `${stop * 100}%`,
-            transform: `translateX(-${stop * 100}%)`,
-          }}
-        >
+    <div className="flex flex-col gap-4">
+      <div
+        ref={gradientRef}
+        className="w-full h-3 rounded-xl relative cursor-copy"
+        style={{
+          background: `linear-gradient(to right, ${colors
+            .map(({ hex, stop }) => `${hex} ${stop * 100}%`)
+            .join(",")})`,
+        }}
+        onClick={handleColorAdd}
+      >
+        {colors.map(({ hex, stop }) => (
           <div
-            className="w-4 h-4 left-1/2 -translate-x-1/2 -top-6 absolute rounded-sm border border-gray-200"
-            style={{ backgroundColor: hex }}
-          />
-        </div>
-      ))}
+            className={`h-8 w-8 top-1/2 flex items-center justify-center absolute bg-gray-200 border hover:border-blue-400 cursor-pointer rounded-full`}
+            style={{
+              transform: `translate(${getTranslate(stop)}px, -50%)`,
+            }}
+          >
+            <span
+              className="w-4 h-4 rounded-full"
+              style={{ backgroundColor: hex }}
+            />
+          </div>
+        ))}
+      </div>
+      <div className="border border-gray-400 rounded-md overflow-hidden">
+        <input type="text" />
+        <input type="color" name="" id="" />
+      </div>
     </div>
   );
 }
